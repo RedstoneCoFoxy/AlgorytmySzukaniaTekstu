7109c3cc3a9a1f7d88bcc40fd9ed6796b3b7e8b8
Stanisław Frąk <S.Frak@student.zschie.edu.pl>
2022-05-22 12:49:50 +0200

    v4

--- AlgorytmySzukaniaTekstu/Form1.cs
+++ AlgorytmySzukaniaTekstu/Form1.cs
@@ -53,6 +53,8 @@ namespace AlgorytmySzukaniaTekstu
                 }
             }
         }
+        DateTime StartTime = DateTime.Now;
+        DateTime EndTime = DateTime.Now;
         bool search_BruteForce()
         {
             int i=0;
@@ -62,7 +64,11 @@ namespace AlgorytmySzukaniaTekstu
                 while(Tekst[a] == Filtr[b]) {
                     a++;
                     b++;
-                    if (b == Filtr.Length)
+                    if (b >= Filtr.Length)
+                    {
+                        break;
+                    }
+                    if (Tekst[a] != Filtr[b])
                     {
                         break;
                     }
@@ -175,16 +181,66 @@ namespace AlgorytmySzukaniaTekstu
             return false;
         }
 
+        bool search_RabinKarp()
+        {
+            int j;
+            int q = 101;
+            int d = 256;
+            int p = 0;
+            int t = 0;
+            int h = 1;
+
+            for (int i = 0; i < Filtr.Length - 1; i++)
+            {
+                h = (h * d) % q;
+            }              
+
+            for (int i = 0; i < Filtr.Length; i++)
+            {
+                p = (d * p + Filtr[i]) % q;
+                t = (d * t + Tekst[i]) % q;
+            }
+
+            for (int i = 0; i <= Tekst.Length - Filtr.Length; i++)
+            {
+                if (p == t)
+                {
+                    for (j = 0; j < Filtr.Length; j++)
+                    {
+                        if (Tekst[i + j] != Filtr[j])
+                        {
+                            break;
+                        }                                            
+                    }
+                    if (j == Filtr.Length)
+                    {
+                        return true;
+                    }
+                    
+                }
+                if (i < Tekst.Length - Filtr.Length)
+                {
+                    t = (d * (t - Tekst[i] * h) + Tekst[i + Filtr.Length]) % q;
+
+                    if (t < 0)
+                    {
+                        t = (t + q);
+                    }
+                  
+                }
+            }
+            return false;
+        }
+
         private void button_startsearch_Click(object sender, EventArgs e)
         {
             if (text_Filter.Text != "")
             {
                 Filtr = text_Filter.Text;
-                DateTime EndTime = DateTime.Parse("01-01-2000");
-                DateTime StartTime = DateTime.Parse("01-01-2000");
-
-                StartTime = DateTime.Now;
                 label_debug.Text = "";
+                StartTime = DateTime.Now;
+
+                
                 label_debug.Text = label_debug.Text + "Czas start: " + StartTime.ToString() + "\n";
 
                 bool found=false;
@@ -192,7 +248,7 @@ namespace AlgorytmySzukaniaTekstu
                 if(radio_bruteforce.Checked==true){found = search_BruteForce();};
                 if(radio_KMP.Checked==true){found = search_KMP(); };
                 if(radio_BoyerMoore.Checked==true){found = search_BoyerMoore();};
-                if(radio_RabinKarp.Checked==true){found = search_BruteForce();};
+                if(radio_RabinKarp.Checked==true){found = search_RabinKarp();};
 
                 if (found)
                 {
@@ -206,8 +262,8 @@ namespace AlgorytmySzukaniaTekstu
                 EndTime = DateTime.Now;
                 label_debug.Text = label_debug.Text + "Czas koniec: " + EndTime.ToString() + "\n";
 
-                TimeSpan span = EndTime - StartTime;
-                label_debug.Text = label_debug.Text + "Różnica: " + span.ToString() +"\n";
+                var roznicaczasu = EndTime - StartTime;
+                label_debug.Text = label_debug.Text + "Różnica: " + roznicaczasu.ToString() +"\n";
             }
             else
             {
